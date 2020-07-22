6d2615f585c45e0a351053845a3556d9feee6a96
NeoFl3x <sutmaxD@gmail.com>
2020-07-22 00:25:03 -0600

    cdmg-web-two- second commit 7/21/2020

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ ScrollReveal().reveal('.work-container', { delay: 250 });
 ScrollReveal().reveal('.education-wrap', { delay: 250 });
 ScrollReveal().reveal('.contact-me', { delay: 250 });
 ScrollReveal().reveal('footer', { delay: 550 });
+
+$(document).ready(function() {
+
+    $('.arrow-up').click(function() {
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 1000);
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('.arrow-up').slideDown(300);
+        } else {
+            $('.arrow-up').slideUp(300);
+        }
+    });
+
+});
